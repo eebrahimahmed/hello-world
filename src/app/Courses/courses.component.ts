59b6e097fe28ad495152042488228508f861eb65
Ibrahim <Ibrahim@IBRAHIM-WORK>
2018-02-17 00:37:21 +0300

    Message

---
+++ src/app/Courses/courses.component.ts
@@ -0,0 +1,14 @@
+  // import { BrowserModule } from '@angular/platform-browser';
+// import { Component } from '@angular/core'
+// import { AppComponent} from '../app.component';
+ 
+import { Component } from '@angular/core'
+@Component({
+  selector: 'courses',
+  templateUrl: './courses.component.html'
+})
+export class CoursesComponent {
+  constructor() {
+
+  }
+}
